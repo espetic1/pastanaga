c103e5a569f5743e2eacdcea5552e029719cefb9
alumnat <alumnat@DESKTOP-KS8A00N>
2019-05-09 08:24:17 +0200

    plantilla

--- login.php
+++ login.php
@@ -17,13 +17,13 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
       $user=$_POST['username'];
       $password=$_POST['password'];
 
-      $sql = "SELECT * FROM AGENDA_USUARI WHERE USUARI = '$user' and CONTRASENYA = '$password'";
+      $sql = "SELECT * FROM PROVA_USUARI WHERE USUARI = '$user' and CONTRASENYA = '$password'";
       $result = mysqli_query($conn,$sql);
       $count = mysqli_num_rows($result);
       
       // If result matched $myusername and $mypassword, table row must be 1 row
       if($count == 1) {
-         header("location: agenda.php");
+         header("location: usuaris.php");
       }else {
          $error = "Contrasenya incorrecta <br>";
       }
@@ -41,78 +41,71 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>Agenda Virtual</title>
+  <title>Pàgina Web de prova TIC</title>
 
   <!-- Bootstrap core CSS -->
   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
   <!-- Custom fonts for this template -->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
-
-  <!-- Plugin CSS -->
-  <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template -->
-  <link href="css/freelancer.min.css" rel="stylesheet">
+  <link href="css/one-page-wonder.css" rel="stylesheet">
 
 </head>
 
-<body id="page-top">
+<body>
 
   <!-- Navigation -->
-  <nav class="navbar navbar-expand-lg bg-secondary fixed-top text-uppercase" id="mainNav">
+  <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
     <div class="container">
-      <a class="navbar-brand js-scroll-trigger" href="#page-top">Agenda Virtual</a>
-      <button class="navbar-toggler navbar-toggler-right text-uppercase bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        Menu
-        <i class="fas fa-bars"></i>
+      <a class="navbar-brand" href="#">Pàgina Web de prova TIC</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto">
-          <li class="nav-item mx-0 mx-lg-1">
-            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="login.php">Log in</a>
+          <li class="nav-item">
+            <a class="nav-link" href="registre.php">Enregistra't</a>
           </li>
-          <li class="nav-item mx-0 mx-lg-1">
-            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="registre.php">Sign up</a>
+          <li class="nav-item">
+            <a class="nav-link" href="login.php">Entra</a>
           </li>
         </ul>
       </div>
     </div>
   </nav>
 
-  <!-- Header -->
-  <header class="masthead bg-primary text-white text-center">
-    <div class="container">
-      
-        
+  <header class="masthead text-center text-white">
+    <div class="masthead-content">
+      <div class="container">
         <form action="login.php" method="post">
-            <input type="text" name="username" placeholder="usuari" /> <br>
-            <input type="password" name="password" placeholder="Contrasenya" /> <br>
-            <input type="submit" value="Entra" /> <br>
+            <input type="text" name="username" class="btn btn-secondary btn-xl rounded-pill mt-5" placeholder="usuari" /> <br>
+            <input type="password" name="password" class="btn btn-secondary btn-xl rounded-pill mt-5" placeholder="Contrasenya" /> <br>
+            <input type="submit" class="btn btn-primary btn-xl rounded-pill mt-5" value="Entra" /> <br>
             <?php echo $error; ?>
         </form>
-        
-        
-        
-      <hr class="star-light">
+      </div>
     </div>
+    <div class="bg-circle-1 bg-circle"></div>
+    <div class="bg-circle-2 bg-circle"></div>
+    <div class="bg-circle-3 bg-circle"></div>
+    <div class="bg-circle-4 bg-circle"></div>
   </header>
+
+
+  <!-- Footer -->
+  <footer class="py-5 bg-black">
+    <div class="container">
+      <p class="m-0 text-center text-white small">Copyright &copy; Your Website 2019</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+
   <!-- Bootstrap core JavaScript -->
   <script src="vendor/jquery/jquery.min.js"></script>
   <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-  <!-- Plugin JavaScript -->
-  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-  <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
-
-  <!-- Contact Form JavaScript -->
-  <script src="js/jqBootstrapValidation.js"></script>
-  <script src="js/contact_me.js"></script>
-
-  <!-- Custom scripts for this template -->
-  <script src="js/freelancer.min.js"></script>
 
 </body>
 
@@ -162,7 +155,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
             <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="login.php">Log in</a>
           </li>
           <li class="nav-item mx-0 mx-lg-1">
-            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="registre.php">Sign up</a>
+            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="registre1.php">Sign up</a>
           </li>
         </ul>
       </div>
@@ -189,7 +182,3 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 
   <!-- Custom scripts for this template -->
   <script src="js/freelancer.min.js"></script>
-
-</body>
-
-</html>
--- registre1.php
+++ registre1.php
@@ -157,7 +157,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
             <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="login.php">Log in</a>
           </li>
           <li class="nav-item mx-0 mx-lg-1">
-            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="registre.php">Sign up</a>
+            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="registre1.php">Sign up</a>
           </li>
         </ul>
       </div>
